964d9cda24f9e4212570c97d5bceeadef1fa4433
David Wauchope <wauchoda@users.noreply.github.com>
2018-01-23 19:11:24 -0400

    Added text input and button to display message as per Assignment 1 Specifications.

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -1,7 +1,13 @@
+/*Used https://developer.android.com/training/basics/firstapp/index.html as reference
+when building this app.*/
 package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(message);
+
     }
 }
